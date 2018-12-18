317e0ca90dbf852c9f4799cff0e691f6a709407b
jmhchang720 <45277077+jmhchang720@users.noreply.github.com>
2018-12-18 22:52:38 +0800

    Add files via upload

---
+++ NewliquidCrystal/examples/LCDHelloWorld/LCDHelloWorld.ino
@@ -0,0 +1,23 @@
+//Hello World program for the LCD20x4 display
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+#define BACKLIGHT_PIN 3
+
+LiquidCrystal_I2C lcd(0x3F,2,1,0,4,5,6,7,3,POSITIVE);  // Set the LCD I2C address
+//LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3,POSITIVE);
+
+void setup () { 
+    lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
+    lcd.setBacklight(HIGH);
+    lcd.begin(20,4);               // initialize the lcd 
+
+    lcd.setCursor(0,0);
+    lcd.clear();
+    lcd.print("Hello World");
+}
+
+void loop () {
+
+}
